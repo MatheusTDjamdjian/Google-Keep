62c838d41f822c1aede1d524d27c31a6f24ed6ce
Matheus <matheustassodjamdjian@gmail.com>
2024-06-07 15:47:38 -0300

    labelsUnique

--- js/labels.js
+++ js/labels.js
@@ -1,10 +1,33 @@
+const EstruNotes = [
+    {
+        id : '',
+        title: '',
+        text: '',
+        controls: [],
+        labels: ["trabalho", "pessoal", "escola"]
+    },
+];
+
+const labelsUnique = [];
+
+notes.forEach(note => {
+    note.labels.forEach(label => {
+        const index = labelsUnique.indexOf(label);
+        if(index === -1){
+            labelsUnique.push(label);
+        }
+    });
+});
+
+console.log(labelsUnique);
+
 // Função para renderizar as labels
-function renderMenuLabels(labels) {
+function renderMenuLabels(labelsUnique) {
     const menuDiv = document.querySelector('.container-menu');
 
     const ul = document.createElement('ul');
 
-    labels.forEach(label => {
+    labelsUnique.forEach(label => {
         const li = document.createElement('li');
         li.textContent = label;
         ul.appendChild(li);
@@ -14,4 +37,4 @@ function renderMenuLabels(labels) {
 }
 
 // Chamar a função para renderizar as labels
-renderMenuLabels(nota.labels);
+renderMenuLabels(notes.labels);
