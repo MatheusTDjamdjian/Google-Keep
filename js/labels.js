11324711fa7f018874ca9a4f48cd462d9a321eba
Matheus <matheustassodjamdjian@gmail.com>
2024-06-07 17:51:19 -0300

    fx labels

--- js/labels.js
+++ js/labels.js
@@ -22,8 +22,30 @@ notes.forEach(note => {
 console.log(labelsUnique);
 
 // Função para renderizar as labels
-function renderMenuLabels(labelsUnique) {
-    const menuDiv = document.querySelector('.container-menu');
+function renderMenuLabels(marcador) {
+    const li = document.createElement('li');    
+    const button = document.createElement('button');    
+    const span = document.createElement('span');
+    const a = document.createElement('a');
+
+    li.classList.add('menu-items');
+    button.classList.add('menu-button');
+    //li.classList.add('marcador');
+    a.href = "arquivo.html";
+    a.classList.add("link-menu");
+    a.textContent = marcador;
+    li.appendChild(button);
+    button.appendChild(a);
+    a.appendChild(span);
+    span.classList.add("material-symbols-outlined");
+    a.appendChild(span);
+    span.classList.add("menu-list-p");
+
+    document.querySelector('#button-menu').insertBefore(li, document.querySelector('#button-menu .menu-items'))
+
+    
+    /*
+    const menuDiv = document.querySelector('.menu-items');
 
     const ul = document.createElement('ul');
 
@@ -33,7 +55,7 @@ function renderMenuLabels(labelsUnique) {
         ul.appendChild(li);
     });
 
-    menuDiv.appendChild(ul);
+    menuDiv.appendChild(ul);*/
 }
 
 // Chamar a função para renderizar as labels
