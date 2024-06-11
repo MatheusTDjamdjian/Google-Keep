c2c0ab8820feba2c801fe9a23434e2cc5076a052
Matheus <matheustassodjamdjian@gmail.com>
2024-06-11 14:50:36 -0300

    Revertendo cod

--- js/labels.js
+++ js/labels.js
@@ -1,13 +1,3 @@
-const EstruNotes = [
-    {
-        id : '',
-        title: '',
-        text: '',
-        controls: [],
-        labels: ["trabalho", "pessoal", "escola"]
-    },
-];
-
 const labelsUnique = [];
 
 notes.forEach(note => {
@@ -22,26 +12,11 @@ notes.forEach(note => {
 console.log(labelsUnique);
 
 // Função para renderizar as labels
-function renderMenuLabels(marcador) {
+function renderMenuLabels(labels) {
     const li = document.createElement('li');    
     const button = document.createElement('button');    
     const span = document.createElement('span');
     const a = document.createElement('a');
-
-    li.classList.add('menu-items');
-    button.classList.add('menu-button');
-    //li.classList.add('marcador');
-    a.href = "arquivo.html";
-    a.classList.add("link-menu");
-    a.textContent = marcador;
-    li.appendChild(button);
-    button.appendChild(a);
-    a.appendChild(span);
-    span.classList.add("material-symbols-outlined");
-    a.appendChild(span);
-    span.classList.add("menu-list-p");
-
-    document.querySelector('#button-menu').insertBefore(li, document.querySelector('#button-menu .menu-items'))
 }
 
 // Chamar a função para renderizar as labels
