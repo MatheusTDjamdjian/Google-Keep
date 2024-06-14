f810fc63c41824b3853f96d9246cddc5dcacee23
Matheus <matheustassodjamdjian@gmail.com>
2024-06-14 14:07:36 -0300

    darkmode.js menu.js

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+// Menu
+
+document.querySelector('#button-menu').addEventListener('click', (event) => {
+    if(document.querySelector('.menu-config').classList.contains('closed')){
+        document.querySelector('.menu-config').classList.remove('closed');
+    } else {
+        document.querySelector('.menu-config').classList.add('closed');
+    }
+});
