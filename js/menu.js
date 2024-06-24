630e66d757c46149ed5e40d0b5ed7e7faac2c1d7
Matheus <matheustassodjamdjian@gmail.com>
2024-06-24 14:08:24 -0300

    fx index.js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-// Menu
-document.querySelector('#button-menu').addEventListener('click', (event) => {
-    if(document.querySelector('.menu-config').classList.contains('closed')){
-        document.querySelector('.menu-config').classList.remove('closed');
-    } else {
-        document.querySelector('.menu-config').classList.add('closed');
-    }
-});
